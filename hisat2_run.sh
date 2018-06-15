c015a37ac2f9f11d3c6c09cec8ff9b498f873164
wolf-adam-eily <36998662+wolf-adam-eily@users.noreply.github.com>
2018-06-14 21:56:56 -0400

    Create hisat2_run.sh

---
+++ hisat2_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=hisat2_run
+#SBATCH --mail-user=
+#SBATCH --mail-type=ALL
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 16
+#SBATCH --mem=120G
+#SBATCH -o hisat2_run_%j.out
+#SBATCH -e hisat2_run_%j.err
+#SBATCH --partition=general
+
+export TMPDIR=/home/CAM/$USER/tmp/
+
+module load hisat2
+
+hisat2 -p 16 --dta -x /isg/shared/databases/alignerIndex/plant/Arabidopsis/thaliana/Athaliana_HISAT2/thaliana -q trimmed_SRR3498212.fastq -S athaliana_root_1.sam
+
+hisat2 -p 16 --dta -x /isg/shared/databases/alignerIndex/plant/Arabidopsis/thaliana/Athaliana_HISAT2/thaliana -q trimmed_SRR3498213.fastq -S athaliana_root_2.sam
+
+hisat2 -p 16 --dta -x /isg/shared/databases/alignerIndex/plant/Arabidopsis/thaliana/Athaliana_HISAT2/thaliana -q trimmed_SRR3498215.fastq -S athaliana_shoot_1.sam
+
+hisat2 -p 16 --dta -x /isg/shared/databases/alignerIndex/plant/Arabidopsis/thaliana/Athaliana_HISAT2/thaliana -q trimmed_SRR3498216.fastq -S athaliana_shoot_2.sam
